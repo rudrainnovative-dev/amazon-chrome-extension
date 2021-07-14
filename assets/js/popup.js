a925ebe4a854915b2896fd0177e970883e377924
Sanjay Singh <sanjay@rudrainnovative.in>
2021-07-14 15:02:39 +0530

    Update extension code v 1.0.0

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,67 +1,70 @@
 document.addEventListener('DOMContentLoaded', function () {
-  	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		$('#openTab').val(tabs[0].url);
-		document.getElementById("asinSelector").focus();
-		if(tabs[0].url.indexOf("amazon") < 0) {
+ 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+		var param = get_parameter(tabs[0].url);  
+		var url = get_hostname(param);
+		if(url == null || url.indexOf("amazon") < 0) {
 			$('.form-input-group').css('display', 'none');
 			$('.error-section').html('DNA Tool work only with amazon website.');
 		}
+		else {
+			$('#amazon_url').val(url);
+		}
+		document.getElementById("asin_selector").focus();
 	});
+
+	chrome.browserAction.setBadgeText({ text: 'DNA' });
+	chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
 });
 
 $(function () {
-	var apiUrl = "https://amz.rudraserver.com";
-    $("#analysisCopy").click(function () {
-    	clearMessage();
-		var openTab = $('#openTab').val();
-		var asin = $("#asinSelector").val();
-		var openTabURL = get_hostname(openTab);
-		if(openTabURL != null && openTabURL.indexOf("amazon") >= 0) {
-			if(asin != '') {
-				$('.spinner-wrapper').show();
-				$.ajax({
-			        url: apiUrl+'/dna',
-			        type: 'POST',
-			        dataType: "json",
-			        data : {action:'reviews', url:openTabURL, asin:asin},
-			        success: function(res) {
-			            if(res.status) {
-			            	var url = apiUrl+'/tmp/dna/'+res.download;
-	                        var a = document.createElement('a');
-	                        a.href = url;
-	                        a.click();
-	                        window.URL.revokeObjectURL(url);
-	                        var asin = $("#asinSelector").val('');
-	                        $('.success-section').html('File downloaded successfully!!!');
-			            }
-			            else {
-			            	$('.error-section').html(res.error);
-			            }
-			            $('.spinner-wrapper').hide();
-			        },
-			        error: function(err) {
-			        	$('.error-section').html('Internal server error!!!');
-			        	$('.spinner-wrapper').hide();
-			        }
-			    });
-			}
-			else {
-				$('#asinSelector').css('border', 'solid 1px #FF0000');
-			}
+	var api_url = "https://amz.rudraserver.com";
+    $("#analysis_copy").click(function () {
+    	clear_message();
+		var amazon_url = $('#amazon_url').val();
+		var asin = $("#asin_selector").val();
+		if(asin != '') {
+			$('.spinner-wrapper').show();
+			$.ajax({
+		        url: api_url+'/dna',
+		        type: 'POST',
+		        dataType: "json",
+		        data : {action : 'reviews', url : amazon_url, asin : asin},
+		        success: function(res) {
+		            if(res.status) {
+		            	var url = api_url+'/tmp/dna/'+res.download;
+                        var a = document.createElement('a');
+                        a.href = url;
+                        a.click();
+                        window.URL.revokeObjectURL(url);
+		            }
+		            else {
+		            	$('.error-section').html(res.error);
+		            }
+		            $('.spinner-wrapper').hide();
+		        },
+		        error: function(err) {
+		        	$('.error-section').html('Internal Server Error!!!');
+		        	$('.spinner-wrapper').hide();
+		        }
+		    });
 		}
 		else {
-			$('.error-section').html('DNA Tool work only with amazon website.');
+				$('#asin_selector').css('border', 'solid 1px #FF0000');
 		}
     });
-
-    function get_hostname(url) {
-	    var m = url.match(/^https:\/\/[^/]+/);
-	    return m?m[0]:null;
-	}
-
-	function clearMessage() {
-		$('.error-section').html('');
-		$('.success-section').html('');
-		$('#asinSelector').css('border', 'solid 1px #ccccc');
-	}
 });
+
+function get_parameter(url) {
+	return url.split('?url=')[1];
+}
+
+function get_hostname(url) {
+    var m = url.match(/^https:\/\/[^/]+/);
+    return m?m[0]:null;
+}
+
+function clear_message() {
+	$('.error-section').html('');
+	$('.success-section').html('');
+	$('#asin_selector').css('border', 'solid 1px #ccccc');
+}
---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+		var left = 800;
+		var top = 100;
+		var width = 380;
+		var height = 400;
+		var tabUrl = "dna_popup.html?url="+tabs[0].url;
+		var popupUrl = chrome.runtime.getURL('dna_popup.html?*');
+		chrome.tabs.query({url: popupUrl},function(tabs) {
+			if(tabs.length == 0) {
+				chrome.tabs.create({ url: chrome.extension.getURL(tabUrl), active: false }, function(tab) { chrome.windows.create({ tabId: tab.id, type: 'popup', 'width': width, 'height': height, 'left': left, 'top': top, focused: true }); });
+			}
+			else{
+				chrome.tabs.remove(tabs[0].id);
+				chrome.tabs.create({ url: chrome.extension.getURL(tabUrl), active: false }, function(tab) { chrome.windows.create({ tabId: tab.id, type: 'popup', 'width': width, 'height': height, 'left': left, 'top': top, focused: true }); });
+			}
+		});
+	});
+});
